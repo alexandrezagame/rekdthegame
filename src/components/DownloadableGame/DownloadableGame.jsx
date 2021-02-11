c8476eb3dde2c4a5c2161e2625711ff99e7af051
JZachrisson <jesper.zachrisson@gmail.com>
2021-02-11 14:11:55 +0100

    add download pdf

--- src/components/DownloadableGame/DownloadableGame.jsx
+++ src/components/DownloadableGame/DownloadableGame.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Button } from '@material-ui/core';
+import { Container, Button, Typography } from '@material-ui/core';
+import useStyles from './styles';
 import pdf from '../../assets/pdf/rekdpdf.pdf';
 
 const DownloadableGame = () => {
+  const classes = useStyles();
+
   return (
-    <Container>
-      <h1>HEY</h1>
-      <p></p>
-      <Button>
-        {' '}
-        <Link to={pdf} target="_blank" download>
+    <Container className={classes.downloadContainer}>
+      <Typography>
+        <h1 className={classes.downloadTitle}>DOWNLOAD THE GAME FOR FREE</h1>
+        <p className={classes.downloadContent}>
+          If you’re too cheap to buy the game or too broke to pay for internet,
+          you can download it for free and live the Chinese sweatshop experience
+          by cutting all 150 cards by hand!
+        </p>
+      </Typography>
+
+      <Button
+        className={classes.downloadButton}
+        variant="contained"
+        color="secondary"
+      >
+        <Link
+          className={classes.downloadLink}
+          to={pdf}
+          target="_blank"
+          download
+        >
           Download
         </Link>
       </Button>
---
+++ src/components/DownloadableGame/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+export default makeStyles(() => ({
+  downloadContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  downloadLink: {
+    textDecoration: 'none',
+    color: 'white',
+  },
+}));
