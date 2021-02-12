3f3eea9b66675ac8d06ef8369655cddf89c9c989
Alexandre Zagame <zagamealexandre@gmail.com>
2021-02-12 11:47:20 +0100

    styling the play online component

--- src/components/PlayOnline/PlayOnline.jsx
+++ src/components/PlayOnline/PlayOnline.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Button, Typography } from '@material-ui/core';
 import useStyles from './styles';
-import pdf from '../../assets/pdf/rekdpdf.pdf';
 import gameImage from '../../assets/logosandpics/game.JPG';
 
 const PlayOnline = () => {
@@ -17,6 +16,11 @@ const PlayOnline = () => {
         <Typography>
           <h1 className={classes.downloadTitle}>PLAY ONLINE</h1>
           <p className={classes.downloadContent}>
+            FRIENDS CAN’T COME OVER? <br></br>
+            COVID GOT YOU ISOLATED? <br></br>
+            WE GOT YOU!
+          </p>
+          <p className={classes.downloadContent}>
             Try our online version, with up to 6 players online from different
             computers! Get a Zoom or Skype meeting going and turn your sofa into
             a virtual party!
@@ -30,7 +34,7 @@ const PlayOnline = () => {
         >
           <Link
             className={classes.downloadLink}
-            to={{pathname: 'http://lets-rekd.herokuapp.com/#/'}}
+            to={{ pathname: 'http://lets-rekd.herokuapp.com/#/' }}
             target="_blank"
           >
             PLAY ONLINE
--- src/components/PlayOnline/styles.js
+++ src/components/PlayOnline/styles.js
@@ -6,20 +6,44 @@ export default makeStyles(() => ({
     flexDirection: 'row',
     alignItems: 'center',
     marginLeft: '5rem',
-    marginRight: '5rem'
+    marginRight: '5rem',
+    '@media (max-width: 780px)': {
+      flexDirection: 'column',
+      flexWrap: 'wrap',
+      marginLeft: '0',
+      marginRight: '0',
+    },
   },
   textContainer: {
-      width: '50%'
+    width: '50%',
+    textAlign: 'center',
+    '@media (max-width: 780px)': {
+      width: '100%',
+    },
+  },
+  downloadTitle: {
+    marginBottom: '1rem',
+  },
+  downloadContent: {
+    marginBottom: '1rem',
   },
   downloadLink: {
     textDecoration: 'none',
     color: 'white',
   },
   image: {
-      width: '400px',
-      height: '400px',
+    width: '400px',
+    height: '400px',
+    '@media (max-width: 780px)': {
+      width: '100%',
+      height: 'auto',
+    },
   },
   imageContainer: {
-width: '50%'
-  }
+    width: '50%',
+    textAlign: 'center',
+    '@media (max-width: 780px)': {
+      width: '100%',
+    },
+  },
 }));
